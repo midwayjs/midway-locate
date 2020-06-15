dd5566dd204606f679234ef2c2c88e555c5f1bb1
Harry Chen <czy88840616@gmail.com>
2020-06-15 15:29:38 +0800

    fix: fix request-promise

--- test/fixtures/rax-fc-sequelize/src/apis/index.ts
+++ test/fixtures/rax-fc-sequelize/src/apis/index.ts
@@ -1,6 +1,7 @@
 import { func, provide } from '@midwayjs/faas';
 import * as p from './p/a';
 import * as sequelize from 'sequelize-typescript';
+import 'request-promise';
 
 @provide()
 @func('index.handler')
